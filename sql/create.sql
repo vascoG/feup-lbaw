32678d9e5c4efef8935dfc9313250ad059061977
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-11-29 15:15:28 +0000

    Updated triggers

--- sql/create.sql
+++ sql/create.sql
@@ -1,12 +1,15 @@
 SET search_path TO lbaw2191;
 
+/*
+  TABLE CREATION START HERE
+*/
+
 DROP TABLE IF EXISTS utilizador CASCADE;
 DROP TABLE IF EXISTS utilizador_ativo CASCADE;
 DROP TABLE IF EXISTS utilizador_banido CASCADE;
 DROP TABLE IF EXISTS apelo_desbloqueio CASCADE;
 DROP TABLE IF EXISTS medalha CASCADE;
 DROP TABLE IF EXISTS notificacao CASCADE;
-DROP TABLE IF EXISTS interacao_utilizador CASCADE;
 DROP TABLE IF EXISTS historico_interacao CASCADE;
 DROP TABLE IF EXISTS resposta CASCADE;
 DROP TABLE IF EXISTS comentario CASCADE;
@@ -16,8 +19,8 @@ DROP TABLE IF EXISTS utilizador_ativo_medalha CASCADE;
 DROP TABLE IF EXISTS questao_seguida CASCADE;
 DROP TABLE IF EXISTS questao_avaliada CASCADE;
 DROP TABLE IF EXISTS resposta_avaliada CASCADE;
-DROP TABLE IF EXISTS tag CASCADE;
-DROP TABLE IF EXISTS questao_tag CASCADE;
+DROP TABLE IF EXISTS etiqueta CASCADE;
+DROP TABLE IF EXISTS questao_etiqueta CASCADE;
 DROP TABLE IF EXISTS palavra_passe CASCADE;
 
 
@@ -58,47 +61,55 @@ CREATE TABLE medalha(
   imagem TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE interacao_utilizador(
+CREATE TABLE questao(
   id SERIAL PRIMARY KEY,
-  texto TEXT UNIQUE NOT NULL,
-  data_publicacao TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL CONSTRAINT data_interacao_ck CHECK (data_publicacao <= now()),
-  autor INTEGER REFERENCES utilizador_ativo(id) ON UPDATE CASCADE
+  texto TEXT NOT NULL,
+  data_publicacao TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  autor INTEGER REFERENCES utilizador_ativo(id),
+  titulo TEXT NOT NULL
+);
+
+CREATE TABLE resposta(
+  id SERIAL PRIMARY KEY,
+  texto TEXT NOT NULL,
+  data_publicacao TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  autor INTEGER REFERENCES utilizador_ativo(id),
+  id_questao INTEGER NOT NULL REFERENCES questao(id),
+  resposta_aceite BOOLEAN NOT NULL
+);
+
+CREATE TABLE comentario(
+  id SERIAL PRIMARY KEY,
+  texto TEXT NOT NULL,
+  data_publicacao TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  autor INTEGER REFERENCES utilizador_ativo(id),
+  id_questao INTEGER REFERENCES questao(id),
+  id_resposta INTEGER REFERENCES resposta(id)
 );
 
 CREATE TABLE notificacao(
   id SERIAL PRIMARY KEY,
   texto TEXT NOT NULL,
-  data TIMESTAMP WITH TIME ZONE NOT NULL CONSTRAINT data_notificao_ck CHECK (data <= now()),
-  interacao INTEGER REFERENCES interacao_utilizador(id) ON UPDATE CASCADE
+  data TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  id_questao INTEGER REFERENCES questao(id),
+  id_comentario INTEGER REFERENCES comentario(id),
+  id_resposta INTEGER REFERENCES resposta(id)
 );
 
 CREATE TABLE historico_interacao(
   id SERIAL PRIMARY KEY,
-  versao INTEGER UNIQUE NOT NULL,
   texto TEXT NOT NULL,
-  data TIMESTAMP WITH TIME ZONE NOT NULL CONSTRAINT data_hostorico CHECK (data <= now()),
-  id_interacao INTEGER NOT NULL REFERENCES interacao_utilizador(id) ON UPDATE CASCADE
-);
+  data TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  id_questao INTEGER REFERENCES questao(id),
+  id_comentario INTEGER REFERENCES comentario(id),
+  id_resposta INTEGER REFERENCES resposta(id)
+  );
 
-CREATE TABLE resposta(
-  id SERIAL PRIMARY KEY,
-  id_interacao INTEGER UNIQUE NOT NULL REFERENCES interacao_utilizador(id) ON UPDATE CASCADE
-);
-
-CREATE TABLE comentario(
-  id SERIAL PRIMARY KEY,
-  id_interacao INTEGER UNIQUE NOT NULL REFERENCES interacao_utilizador(id) ON UPDATE CASCADE
-);
-
-CREATE TABLE questao (
-  id SERIAL PRIMARY KEY,
-  id_interacao INTEGER UNIQUE NOT NULL REFERENCES interacao_utilizador(id) ON UPDATE CASCADE,
-  titulo TEXT NOT NULL
-);
 
 CREATE TABLE utilizador_ativo_notificacao(
   id_utilizador INTEGER NOT NULL REFERENCES utilizador_ativo(id) ON UPDATE CASCADE,
   id_notificacao INTEGER NOT NULL REFERENCES notificacao(id) ON UPDATE CASCADE,
+  dataLida TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
   PRIMARY KEY (id_utilizador, id_notificacao)
 );
 
@@ -126,13 +137,92 @@ CREATE TABLE resposta_avaliada(
   PRIMARY KEY (id_utilizador, id_resposta)
 );
 
-CREATE TABLE tag(
+CREATE TABLE etiqueta(
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL
 );
 
-CREATE TABLE questao_tag(
-  id_tag INTEGER NOT NULL REFERENCES tag(id) ON UPDATE CASCADE,
+CREATE TABLE questao_etiqueta(
+  id_etiqueta INTEGER NOT NULL REFERENCES etiqueta(id) ON UPDATE CASCADE,
   id_questao INTEGER NOT NULL REFERENCES questao(id) ON UPDATE CASCADE,
-  PRIMARY KEY (id_questao, id_tag)
+  PRIMARY KEY (id_questao, id_etiqueta)
 );
+
+/*
+
+  TRIGGERS START HERE
+
+*/
+
+DROP INDEX IF EXISTS pesquisa_questao;
+DROP INDEX IF EXISTS auto_interacao_comentario;
+
+
+CREATE OR REPLACE FUNCTION auto_interacao_resposta() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+  IF EXISTS(SELECT * FROM questao WHERE NEW.id_questao = id_questao AND NEW.autor = autor) THEN
+  RAISE EXCEPTION 'O utilizador não pode responder à própria questão!';
+  END IF;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER auto_interacao_resposta
+  BEFORE INSERT ON resposta
+  FOR EACH ROW
+  EXECUTE PROCEDURE auto_interacao_resposta();
+
+
+CREATE FUNCTION auto_interacao_comentario() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+  IF EXISTS(SELECT * FROM questao WHERE NEW.id_questao = id_questao AND NEW.autor = autor) THEN
+  RAISE EXCEPTION 'O utilizador não pode comentar à própria questão!';
+  END IF;
+  IF EXISTS(SELECT * FROM resposta WHERE NEW.id_resposta = id_resposta AND NEW.autor = autor) THEN
+  RAISE EXCEPTION 'O utilizador não pode comentar à própria responder!';
+  END IF;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER auto_interacao_comentario
+  BEFORE INSERT ON comentario
+  FOR EACH ROW
+  EXECUTE PROCEDURE auto_interacao_comentario();
+
+
+/*
+  TRIGGER PARA PESQUISA EM QUESTÕES
+*/
+
+ALTER TABLE questao
+ADD COLUMN IF NOT EXISTS tsvectors TSVECTOR;
+
+CREATE OR REPLACE FUNCTION atualiza_tsvector_conteudo_questao() RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        NEW.tsvectors = (
+            setweight(to_tsvector('portuguese', NEW.titulo), 'A'),
+            setweight(to_tsvector('portuguese', NEW.texto), 'B')
+        );
+    END IF;
+    IF TG_OP = 'UPDATE' THEN
+        IF (NEW.titulo <> OLD.titulo OR NEW.titulo <> NEW.texto) THEN
+            NEW.tsvectors = (
+                setweight(to_tsvector('portuguese', NEW.titulo), 'A'),
+                setweight(to_tsvector('portuguese', NEW.texto), 'B')
+            );
+        END IF;
+    END IF;
+    RETURN NEW;
+END $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER pesquisa_questao
+    BEFORE INSERT OR UPDATE ON questao
+    FOR EACH ROW
+    EXECUTE PROCEDURE atualiza_tsvector_conteudo_questao();
+
+CREATE INDEX pesquisa_questao ON questao USING GIN (tsvectors);
