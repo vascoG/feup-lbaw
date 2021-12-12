1b2ce92ff066a74d8227900a6c6832d7fd942cb6
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-12-12 18:11:41 +0000

    Utilizador criado

---
+++ app/Models/Utilizador.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticable;
+
+class Utilizador extends Authenticable {
+    public $timestamps = false;
+
+    protected $table = 'user';
+    
+    protected $fillable = [
+        'imagem_perfil', 'nome_utilizador', 'nome', 'e_mail', 'data_nascimento', 'id_palavra_passe'
+    ];
+
+    protected $attributes = [
+        'moderador' => false,
+        'administrador' => false,
+    ];
+
+    protected $hidden = [
+        'id_palavra_passe', 'imagem_perfil'
+    ];
+
+    public function palavraPasse() {
+        return $this->hasOne(App\Models\PalavraPasse, 'id_palavra_passe');
+    }
+}
