2a41b2d523bce85f01dbd7933d17f0d9fa4ae817
Vasco Gomes <up201906617@fe.up.pt>
2021-12-17 15:29:12 +0000

    Página de criar questão simplista criada. Falta implementar tags e submissão.

--- resources/views/pages/criarquestao.blade.php
+++ resources/views/pages/criarquestao.blade.php
@@ -3,6 +3,27 @@
 @section('conteudo')
 <form method = "POST" action="{{route('criarquestao')}}" id="questao-form">
     {{ csrf_field() }}
+    <div class="form-group ">
+        <label for="titulo">Título da questão</label>
+        <input required type="text" class="form-control questao-titulo" id="tituloQuestao" name="titulo" placeholder="Título">
+    </div>
+    <div class="form-group ">
+        <label for="texto">Corpo da questão</label>
+        <textarea required class="form-control questao-texto" id="textoQuestao" name="texto" placeholder="Escreva aqui a sua questão"></textarea>
+    </div>
+    <div class="form-group ">
+        <label for="etiqueta">Tags</label>
+        <textarea class="form-control questao-titulo" id="etiquetaQuestao" name="etiqueta" placeholder="Ainda não está implementado..."></textarea>
+    </div>
+    <div >
+        <button class="btn float-end clearfix rounded-pill questao-button" type="submit" id="submit_button">
+            <b>
+                CRIAR QUESTÃO
+            </b>
+        </button>
+    </div>
+
+</form>
 
 
 @endsection
