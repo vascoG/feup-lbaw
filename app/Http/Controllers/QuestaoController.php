3f0de0df9d5d47b5dd2fabe5b74e1892528ff526
Vasco Gomes <up201906617@fe.up.pt>
2021-12-17 12:24:20 +0000

    Editar questao controller not completed

--- app/Http/Controllers/QuestaoController.php
+++ app/Http/Controllers/QuestaoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Etiqueta;
+use App\Models\HistoricoInteracao;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -50,10 +51,50 @@ class QuestaoController extends Controller
         //se nao existir a etiqueta que ele escreveu?
         $questao->etiquetas()->attach(Etiqueta::where('nome',$request->get('etiqueta'))->first()->id);
         $questao->save();
-        return redirect()->route('questao',[$questao]);
+        return redirect()->route('questao',[$questao->id]);
     }
 
+    /**
+     * Mostra o formulário para editar uma questão
+     * @return Response
+     */
+    public function showEditForm($idQuestao)
+    {
+        if(!Auth::check()) return redirect('/login');
+        $questao = Questao::find($idQuestao);
+        $this->authorize('editar',$questao);
+        return view('pages.editarQuestao',['questao'=>$questao]);
+    }
+    /**
+     * Edita uma questão
+     * @return Questao A questao editada
+     */
+    public function edit(Request $request, $idQuestao)
+    {
+        if(!Auth::check()) return redirect('/login');
+        $questao = Questao::find($idQuestao);
+        $this->authorize('editar',$questao);
+        $validator = Validator::make($request->all(),
+            [
+                'titulo' => 'required|max:100',
+                'texto' => 'required',
+                'etiqueta' => 'required',
+            ]);
+        if($validator->fails())
+        {
+            return redirect()->route('editar-questao',[$idQuestao])->withErrors($validator);
+        }
+        
+        $questao->etiquetas()->attach(Etiqueta::where('nome',$request->get('etiqueta'))->first()->id);
 
-
+        //caso nao edite nada, nao deve adicionar historico
+        $historicoQuestao = new HistoricoInteracao([
+            'texto' => $questao->texto,
+            'id_questao' => $idQuestao,
+        ]);
+        $historicoQuestao->save();
+        $questao->save();
+        return redirect()->route('questao',[$idQuestao]);
+    }
 
 }
--- app/Policies/QuestaoPolicy.php
+++ app/Policies/QuestaoPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\Questao;
 use App\Models\UtilizadorAtivo;
 use App\Models\UtilizadorBanido;
+use App\Models\Utilizador;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,5 +19,10 @@ class QuestaoPolicy{
         return ($user_banned==null);
     }
 
+    public function editar(UtilizadorAtivo $utilizadorAtivo, Questao $questao)
+    {   
+        $user = Utilizador::find($utilizadorAtivo->id_utilizador);
+        return $utilizadorAtivo->id_utilizador == $questao->autor || $user->administrador;
+    }
 
 }
