76d2856e170a76dab08207d1b2302afadd4f425d
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-12-20 11:33:11 +0000

    Pagina de registo estilizada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,12 +2,12 @@
 
 @section('conteudo')
 <div class="d-flex justify-content-center pt-4">
-    <form method="POST" action="{{ route('login') }}" id="login-form">
+    <form method="POST" action="{{ route('login') }}" class="form-autenticacao">
         {{ csrf_field() }}
 
-        <div class="form-group login-field">
-            <label for="e_mail" class="form-label">Email</label>
-            <input id="e_mail" type="email" name="e_mail" value="{{ old('e_mail') }}" class="form-control" required autofocus>
+        <div class="form-group campo-form-autenticacao">
+            <label for="email" class="form-label">Email</label>
+            <input id="email" type="email" name="e_mail" value="{{ old('e_mail') }}" class="form-control" required autofocus>
         </div>
         @if ($errors->has('e_mail'))
             <span class="erro-input">
@@ -15,9 +15,9 @@
             </span>
         @endif
         
-        <div class="form-group login-field">
-            <label for="palavra_passe" class="form-label">Palavra-passe</label>
-            <input id="palavra_passe" type="password" name="palavra_passe" class="form-control" required>
+        <div class="form-group campo-form-autenticacao">
+            <label for="palavra-passe" class="form-label">Palavra-passe</label>
+            <input id="palavra-passe" type="password" name="palavra_passe" class="form-control" required>
             @if ($errors->has('palavra_passe'))
                 <span class="erro-input">
                     {{ $errors->first('palavra_passe') }}
@@ -25,14 +25,14 @@
             @endif
         </div>
 
-        <div class="form-check login-field">
+        <div class="form-check campo-form-autenticacao">
             <label for="login-lembrar" class="form-check-label">Lembrar-me</label>
             <input type="checkbox" name="lembrar" id="login-lembrar" class="form-check-input shadow-none cb-cor-diferente" {{ old('lembrar') ? 'checked' : '' }}>
         </div>
 
-        <div class="login-field" id="botoes-login">
-            <a id="login-registo" class="btn btn-secondary px-2" href="{{ route('registo') }}">Registe-se</a>
-            <button type="submit" id="login-submeter" class="btn btn-primary px-4">Login</button>
+        <div class="campo-form-autenticacao" id="botoes-login">
+            <a class="btn btn-secondary px-2 autenticacao-secundario" href="{{ route('registo') }}">Registe-se</a>
+            <button type="submit" class="btn btn-primary px-4 autenticacao-primario">Login</button>
         </div>
     </form>
 </div>
--- resources/views/auth/registo.blade.php
+++ resources/views/auth/registo.blade.php
@@ -1,44 +1,64 @@
 @extends('layouts.minimo')
 
 @section('conteudo')
-<div class="h-100 d-flex flex-row flex-nowrap overflow-hidden">
-  <form id="form-registo" method="POST" action="{{ route('registo') }}">
+<div class="d-flex justify-content-center pt-4">
+  <form id="login-form" class="form-autenticacao" method="POST" action="{{ route('registo') }}">
     {{ csrf_field() }}
 
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @if ($errors->has('name'))
-      <span class="error">
-          {{ $errors->first('name') }}
-      </span>
-    @endif
+    <div class="form-group campo-form-autenticacao">
+      <label for="nome-utilizador" class="form-label">Nome de utilizador</label>
+      <input id="nome-utilizador" class="form-control" type="text" name="nome_utilizador" value="{{ old('nome_utilizador') }}" required autofocus>
+      @if ($errors->has('nome_utilizador'))
+        <span class="error">
+            {{ $errors->first('nome_utilizador') }}
+        </span>
+      @endif
+    </div>
 
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-      <span class="error">
-          {{ $errors->first('email') }}
-      </span>
-    @endif
+    <div class="form-group campo-form-autenticacao">
+      <label for="nome" class="form-label">Nome de apresentação</label>
+      <input id="nome" class="form-control" type="text" name="nome" value="{{ old('nome') }}" required autofocus>
+      @if ($errors->has('nome'))
+        <span class="error">
+            {{ $errors->first('nome') }}
+        </span>
+      @endif
+    </div>
 
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-      <span class="error">
-          {{ $errors->first('password') }}
-      </span>
-    @endif
+    <div class="form-group campo-form-autenticacao">
+      <label for="email" class="form-label">Endereço de E-Mail</label>
+      <input id="email" class="form-control" type="email" name="e_mail" value="{{ old('e_mail') }}" required>
+      @if ($errors->has('e_mail'))
+        <span class="error">
+            {{ $errors->first('e_mail') }}
+        </span>
+      @endif
+    </div>
 
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
+    <div class="form-group campo-form-autenticacao">
+      <label for="data-nascimento" class="form-label">Data de nascimento</label>
+      <input id="data-nascimento" class="form-control" type="date" name="data_nascimento" value="{{ old('data_nascimento') }}"required>
+    </div>
 
-    <button type="submit">
-      Register
-    </button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
+    <div class="form-group campo-form-autenticacao">
+      <label for="palavra_passe" class="form-label">Palavra-passe</label>
+      <input id="palavra_passe" class="form-control" type="password" name="palavra_passe" required>
+      @if ($errors->has('palavra_passe'))
+        <span class="error">
+            {{ $errors->first('palavra_passe') }}
+        </span>
+      @endif
+    </div>
+
+    <div class="form-group campo-form-autenticacao">
+      <label for="palavra-passe-confirmacao" class="form-label">Confirmação da palavra-passe</label>
+      <input id="palavra-passe-confirmacao" class="form-control" type="password" name="palavra_passe_confirmacao" required>
+    </div>
+
+    <div class="campo-form-autenticacao" id="botoes-login">
+      <a class="btn btn-secondary px-4 autenticacao-secundario" href="{{ route('login') }}">Login</a>
+      <button type="submit" class="btn btn-primary px-2 autenticacao-primario">Registar</button>
+    </div>
   </form>
-  <div id="registo-img-div">
-    <img id="registo-img" src="{{ asset('images/registo.jpg') }}">
-  </div>
 </div>
 @endsection
