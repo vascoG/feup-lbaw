3a0d5ac837baba025596b837af964a0e692106e8
Mariana Monteiro <up202003480@fe.up.pt>
2021-12-22 11:59:55 +0000

    Acabei página sobre nós

--- resources/views/pages/sobrenos.blade.php
+++ resources/views/pages/sobrenos.blade.php
@@ -8,4 +8,45 @@
   <p>Este site está disponível para qualquer pessoa com interessa na área e deve ser utilizado colaborativamente.</p>
 </div>
 
+<h2 style="text-align:center">A Nossa Equipa</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="vasco.png" alt="Vasco Gomes" style="width:100%">
+      <div class="container">
+        <h2>Vasco Gomes</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Descrição da pessoa.</p>
+        <p>vasco@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="mariana.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mariana Monteiro</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Descrição da pessoa.</p>
+        <p>mariana@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="francisco.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Francisco Oliveira </h2>
+        <p class="title">CEO & Founder</p>
+        <p>Descrição da pessoa.</p>
+        <p>francisco@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
