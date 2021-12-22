fb44c1e5324c7d60cdc7e676ccd48cf520792d5d
Mariana Monteiro <up202003480@fe.up.pt>
2021-12-22 13:13:17 +0000

    Fiz as páginas estáticas faq e contactos

--- resources/views/pages/sobrenos.blade.php
+++ resources/views/pages/sobrenos.blade.php
@@ -2,7 +2,7 @@
 
 @section('conteudo')
 
-<div class="about-section">
+<div class="pagina-estaticas">
   <h1>Sobre nós</h1>
   <p>FinJet é um site que nasceu em 2021 de forma a ajudar os nossos utilizadores na área das finanças pessoais.</p>
   <p>Este site está disponível para qualquer pessoa com interessa na área e deve ser utilizado colaborativamente.</p>
