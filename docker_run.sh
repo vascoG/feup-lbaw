2c7ebd65edf5a45531f0c2ea8124f8f5abdb55b3
Francisco Oliveira <up201907361@edu.fe.up.pt>
2021-12-26 11:04:55 +0000

    Atualizei scripts de configuracao para suportar gestao de imagens

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd /var/www; php artisan config:cache
+cd /var/www; php artisan config:cache; rm -f public/storage/; php artisan storage:link 
 env >> /var/www/.env
 php-fpm8.0 -D
 nginx -g "daemon off;"
