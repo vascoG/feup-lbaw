fa4dc8660fe0e66eb65c7cc55f7dd66ad74a2e30
Francisco Oliveira <up201907361@edu.fe.up.pt>
2022-01-05 17:05:58 +0000

    Formulario de resposta centrado

--- resources/views/pages/editarresposta.blade.php
+++ resources/views/pages/editarresposta.blade.php
@@ -1,31 +1,30 @@
 @extends('layouts.minimo')
 
 @section('conteudo')
-<form method = "POST" action="{{route('edit-resposta',[$questao->id, $resposta->id])}}" id="resposta-editar-form">
-    {{ csrf_field() }}
-    @method('PUT')
-    <div class="form-group questao-titulo">
-        <label for="texto">Corpo da resposta</label>
-        <textarea required class="form-control questao-texto" id="textoResposta" name="texto" >{{$resposta->texto}}</textarea>
-    </div>
+<div class="w-75 mx-auto mt-4">
+    <form method = "POST" action="{{route('edit-resposta',[$questao->id, $resposta->id])}}" id="resposta-editar-form">
+        {{ csrf_field() }}
+        @method('PUT')
+        <div class="form-group">
+            <label for="texto">Corpo da resposta</label>
+            <textarea required class="form-control questao-texto" id="textoResposta" name="texto" >{{$resposta->texto}}</textarea>
+        </div>
+    </form>
+    <form method = "POST" action="{{route('eliminar-resposta',[$questao->id, $resposta->id])}}" id="questao-eliminar-form">
+        {{ csrf_field() }}
+        @method('DELETE')
+    </form>
     <div>
-        <button class="btn clearfix rounded-pill questao-button float-end" type="submit" id="submit_button">
+        <button class="btn clearfix rounded-pill questao-button float-end" type="submit" form="resposta-editar-form" id="submit_button">
             <b>
                 EDITAR RESPOSTA
             </b>
         </button>
-        </form>
-        <form method = "POST" action="{{route('eliminar-resposta',[$questao->id, $resposta->id])}}" id="questao-eliminar-form">
-        {{ csrf_field() }}
-        @method('DELETE')
-        <button class="btn clearfix rounded-pill eliminar-button float-end" type="submit" id="submit_button">
+        <button class="btn clearfix rounded-pill eliminar-button float-end" type="submit" form="questao-eliminar-form" id="submit_button">
             <b>
                 ELIMINAR RESPOSTA
             </b>
         </button>
-        </form>
+    </div>
 </div>
-
-
-
 @endsection
