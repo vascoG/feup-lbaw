27a781c4a15f36fd6f31375844840440b09f965d
Francisco Oliveira <up201907361@edu.fe.up.pt>
2022-01-21 12:53:13 +0000

    Resolvi bug em que respostas tinham todas o mesmo id

--- app/Scopes/RespostaVisivelScope.php
+++ app/Scopes/RespostaVisivelScope.php
@@ -11,7 +11,8 @@ use Illuminate\Database\Eloquent\Scope;
 class RespostaVisivelScope implements Scope
 {
     public function apply(Builder $builder, Model $model) {
-        $questoesVisiveis = Questao::query();
+        $questoesVisiveis = Questao::query()
+            ->select('id AS id_questao');
         $ativosBanidos = UtilizadorAtivo::query()
             ->select('utilizador_ativo.id_utilizador AS id_utilizador')
             ->join('utilizador_banido', 'utilizador_banido.id_utilizador', '=', 'utilizador_ativo.id_utilizador');
@@ -19,9 +20,9 @@ class RespostaVisivelScope implements Scope
             $join->on('utilizadores_ativos_banidos.id_utilizador', '=', 'resposta.autor');
         })
         ->leftJoinSub($questoesVisiveis, 'questoes_visiveis', function($join) {
-            $join->on('questoes_visiveis.id', '=', 'resposta.id_questao');
+            $join->on('questoes_visiveis.id_questao', '=', 'resposta.id_questao');
         })
-        ->whereNotNull('questoes_visiveis.id')
+        ->whereNotNull('questoes_visiveis.id_questao')
         ->whereNull('utilizadores_ativos_banidos.id_utilizador');
     }
 }
